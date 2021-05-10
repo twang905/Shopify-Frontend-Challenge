f4c9184b60045fd9aa7f83c74324597e76d75956
Thomas Wang <twang905@gmail.com>
2021-05-09 20:15:22 -0400

    fix poster without nomination

--- src/components/MoviesSection.jsx
+++ src/components/MoviesSection.jsx
@@ -4,7 +4,7 @@ export default function MoviesSection(props) {
   // lists all movies from search inquiry
   return(
     <div className="h-full overflow-scroll bg-black">
-      <div className="grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-2 py-2 lg:max-w-7xl m-auto">
+      <div className="grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-4 py-4 lg:max-w-7xl m-auto">
         {props.movies.map((movie) => 
           <MovieCard
             movie={movie}
--- src/components/Nomination.jsx
+++ src/components/Nomination.jsx
@@ -3,13 +3,20 @@ import { removeMovie } from "../actions"
 
 export default function Nomination(props) {
   const dispatch = useDispatch();
+  const getPoster = () => {
+    return props.movie.Poster !== "N/A"
+    ? <img src={`${props.movie.Poster}`} className="h-44 rounded-md w-auto" />
+    : <div className="h-44 w-32 rounded-md bg-vs-grey-2 flex items-center justify-center p-4">
+        {props.movie.Title}
+      </div>
+  }
   // clickable movie poster to show nominated movie
   return(
     <button 
       className="px-1"
       onClick={() => dispatch(removeMovie(props.movie))}
     >
-      <img src={`${props.movie.Poster}`} className="h-44 rounded-md w-auto" />
+      {getPoster()}
     </button>
   )
 }
